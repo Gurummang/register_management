19d2e7915b3ba5bae2c8742d4bbf0e7f66292055
mandeuk <mandeuk1999@gmail.com>
2024-07-14 21:09:37 +0900

    KAN-193--saas-crud / Feat: repo class

--- src/main/java/GASB/register_management/repository/SaasRepository.java
+++ src/main/java/GASB/register_management/repository/SaasRepository.java
@@ -1,4 +1,7 @@
 package GASB.register_management.repository;
 
-public interface SaasRepository {
+import GASB.register_management.entity.Saas;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SaasRepository extends JpaRepository<Saas, Integer> {
 }
