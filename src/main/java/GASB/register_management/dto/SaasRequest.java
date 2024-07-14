6eb65560c030bfb69bc79d5b046ccabb3055adab
mandeuk <mandeuk1999@gmail.com>
2024-07-14 21:07:49 +0900

    KAN-193--saas-crud / Feat: dto classes

--- src/main/java/GASB/register_management/dto/SaasRequest.java
+++ src/main/java/GASB/register_management/dto/SaasRequest.java
@@ -1,4 +1,14 @@
 package GASB.register_management.dto;
 
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class SaasRequest {
+
+    private String action;  // POST 구분용
+    private Integer id;
+    private String saas_name;
 }
--- src/main/java/GASB/register_management/dto/SaasResponse.java
+++ src/main/java/GASB/register_management/dto/SaasResponse.java
@@ -1,4 +1,22 @@
 package GASB.register_management.dto;
 
 public class SaasResponse {
+
+    private String status;
+    private String messeage;
+    private Integer id;
+    private String saas_name;
+
+    // Response for POST
+    public SaasResponse(String status, String messeage, Integer id) {
+        this.status = status;
+        this.messeage = messeage;
+        this.id = id;
+    }
+
+    // Response for GET
+    public SaasResponse(Integer id, String saas_name){
+        this.id = id;
+        this.saas_name = saas_name;
+    }
 }
