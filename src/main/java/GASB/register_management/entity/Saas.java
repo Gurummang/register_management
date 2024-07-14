cd09e855e043e73f45c3fd323efe639b08053ac6
mandeuk <mandeuk1999@gmail.com>
2024-07-14 21:08:21 +0900

    KAN-193--saas-crud / Feat: entity class

--- src/main/java/GASB/register_management/entity/Saas.java
+++ src/main/java/GASB/register_management/entity/Saas.java
@@ -1,4 +1,17 @@
 package GASB.register_management.entity;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "saas")
 public class Saas {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String saas_name;
 }
