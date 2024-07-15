6a692b3bd7d736dd1dbdec3d07e78add006a29ba
mandeuk <mandeuk1999@gmail.com>
2024-07-15 14:12:16 +0900

    KAN-193--saas-crud / Fix: Don't allow NULL for Req&Resp

--- src/main/java/GASB/register_management/dto/SaasResponse.java
+++ src/main/java/GASB/register_management/dto/SaasResponse.java
@@ -1,11 +1,13 @@
 package GASB.register_management.dto;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Getter;
 import lombok.Setter;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Getter
 @Setter
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class SaasResponse {
 
     private String status;
