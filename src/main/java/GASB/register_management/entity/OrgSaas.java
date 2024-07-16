bbfaf08b0e6da76d88e25a431df188a9ebbbd7da
mandeuk <mandeuk1999@gmail.com>
2024-07-16 23:31:44 +0900

    KAN-197-regi-crud / Feat: entity, OrgSaas & OrgSaasConfig

--- src/main/java/GASB/register_management/entity/OrgSaas.java
+++ src/main/java/GASB/register_management/entity/OrgSaas.java
@@ -1,2 +1,21 @@
-package GASB.register_management.entity;public class OrgSaas {
+package GASB.register_management.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "org_saas")
+public class OrgSaas {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private Integer org_id;
+    private Integer saas_id;
+    private Integer config_file;
+    private Integer status;
+    private Integer security_score;
 }
--- src/main/java/GASB/register_management/entity/OrgSaasConfig.java
+++ src/main/java/GASB/register_management/entity/OrgSaasConfig.java
@@ -1,2 +1,25 @@
-package GASB.register_management.entity;public class OrgSaasConfig {
+package GASB.register_management.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.sql.Timestamp;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "saas_config")
+public class OrgSaasConfig {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String saas_admin_email;
+    private String api_key;
+    private String webhook_url;
+    private String validation;
+    private String nickname;
+    private String saas_alias;
+    private Timestamp register_date;
 }
