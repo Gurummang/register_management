5fa5ceff01c5f9be98426b2b45b96d7be7261aca
mandeuk <mandeuk1999@gmail.com>
2024-07-16 23:32:25 +0900

    KAN-197-regi-crud / Feat: repositoy, OrgSaas & OrgSaasConfig

--- src/main/java/GASB/register_management/repository/OrgSaasConfigRepository.java
+++ src/main/java/GASB/register_management/repository/OrgSaasConfigRepository.java
@@ -1,2 +1,10 @@
-package GASB.register_management.repository;public interface OrgSaasConfigRepository {
+package GASB.register_management.repository;
+
+import GASB.register_management.entity.OrgSaas;
+import GASB.register_management.entity.OrgSaasConfig;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface OrgSaasConfigRepository extends JpaRepository<OrgSaasConfig, Long> {
 }
--- src/main/java/GASB/register_management/repository/OrgSaasRepository.java
+++ src/main/java/GASB/register_management/repository/OrgSaasRepository.java
@@ -1,2 +1,9 @@
-package GASB.register_management.repository;public interface OrgSaasRepository {
+package GASB.register_management.repository;
+
+import GASB.register_management.entity.OrgSaas;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface OrgSaasRepository extends JpaRepository<OrgSaas, Integer> {
 }
