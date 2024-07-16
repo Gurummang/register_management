d1f43f0e361ea75f4cd6a704d8ccefa8a67e7b82
mandeuk <mandeuk1999@gmail.com>
2024-07-16 23:30:22 +0900

    KAN-197-regi-crud / Feat: controller

--- src/main/java/GASB/register_management/controller/OrgSaasController.java
+++ src/main/java/GASB/register_management/controller/OrgSaasController.java
@@ -1,2 +1,23 @@
-package GASB.register_management.controller;public class OrgSaasController {
+package GASB.register_management.controller;
+
+import GASB.register_management.dto.OrgSaasRequest;
+import GASB.register_management.dto.OrgSaasResponse;
+import GASB.register_management.service.OrgSaasService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/org-saas")
+public class OrgSaasController {
+
+    @Autowired
+    private OrgSaasService orgSaasService;
+
+    @GetMapping("/{saasId}/mkurl")
+    public OrgSaasResponse mkUrl(@PathVariable Integer saasId) {
+        return orgSaasService.getUrl(saasId);
+    }
 }
