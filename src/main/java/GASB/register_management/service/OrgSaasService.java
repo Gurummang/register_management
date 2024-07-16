2603f703de5864ede84a27e815f6d9b05cbb7f96
mandeuk <mandeuk1999@gmail.com>
2024-07-16 23:32:41 +0900

    KAN-197-regi-crud / Feat: service

--- src/main/java/GASB/register_management/service/OrgSaasService.java
+++ src/main/java/GASB/register_management/service/OrgSaasService.java
@@ -1,2 +1,13 @@
-package GASB.register_management.service;public interface OrgSaasService {
+package GASB.register_management.service;
+
+import GASB.register_management.dto.OrgSaasRequest;
+import GASB.register_management.dto.OrgSaasResponse;
+import java.util.List;
+
+public interface OrgSaasService {
+
+    OrgSaasResponse registerOrgSaas(OrgSaasRequest orgSaasRequest);
+    OrgSaasResponse modifyOrgSaas(OrgSaasRequest orgSaasRequest);
+    OrgSaasResponse deleteOrgSaas(OrgSaasRequest orgSaasRequest);
+    List<OrgSaasResponse> getOrgSaasList();
 }
--- src/main/java/GASB/register_management/service/imple/OrgSaasServiceImple.java
+++ src/main/java/GASB/register_management/service/imple/OrgSaasServiceImple.java
@@ -1,2 +1,50 @@
-package GASB.register_management.service.imple;public class OrgSaasServiceImple {
+package GASB.register_management.service.imple;
+
+import GASB.register_management.dto.OrgSaasRequest;
+import GASB.register_management.dto.OrgSaasResponse;
+import GASB.register_management.service.OrgSaasService;
+import GASB.register_management.entity.OrgSaas;
+import GASB.register_management.entity.OrgSaasConfig;
+import GASB.register_management.repository.OrgSaasRepository;
+import GASB.register_management.repository.OrgSaasConfigRepository;
+import GASB.register_management.entity.Saas;
+import GASB.register_management.repository.SaasRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+import java.util.UUID;
+
+@Service
+public class OrgSaasServiceImple implements OrgSaasService {
+
+    @Autowired
+    private OrgSaasRepository orgSaasRepository;
+    @Autowired
+    private OrgSaasConfigRepository orgSaasConfigRepository;
+
+    @Override
+    public OrgSaasResponse registerOrgSaas(OrgSaasRequest orgSaasRequest) {
+        return null;
+    }
+
+    @Override
+    public OrgSaasResponse modifyOrgSaas(OrgSaasRequest orgSaasRequest) {
+        return null;
+    }
+
+    @Override
+    public OrgSaasResponse deleteOrgSaas(OrgSaasRequest orgSaasRequest) {
+        return null;
+    }
+
+    @Override
+    public List<OrgSaasResponse> getOrgSaasList() {
+        return List.of();
+    }
 }
