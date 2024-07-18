6910f6f7f691ea9d33435b042a1a26b9ecd51fea
mandeuk <mandeuk1999@gmail.com>
2024-07-18 10:23:05 +0900

    KAN-197-regi-crud / Feat: modify func, but small error

--- src/main/java/GASB/register_management/service/imple/OrgSaasServiceImple.java
+++ src/main/java/GASB/register_management/service/imple/OrgSaasServiceImple.java
@@ -8,12 +8,14 @@ import GASB.register_management.entity.Workspace;
 import GASB.register_management.repository.OrgSaasRepository;
 import GASB.register_management.repository.WorkspaceRepository;
 
+import org.hibernate.jdbc.Work;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.sql.Timestamp;
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class OrgSaasServiceImple implements OrgSaasService {
@@ -28,36 +30,76 @@ public class OrgSaasServiceImple implements OrgSaasService {
         OrgSaas orgSaas = new OrgSaas();
         Workspace workSpace = new Workspace();
 
+//        < Req >
+//        {
+//            "org_id": int,                  // FE가 입력 // 세션에 있을듯?
+//            "saas_id": int,                 // 등록할 때 받아서 POST
+//            "space_id": string,             // space_id랑 뭐가 다르지?
+//            "workspace_name": string,       // API로 얻어서 입력 // 서현이가? // valid때 내가 할수도? // client가 입력할수도?
+//            "saas_admin_email": string,
+//            "webhook_url": string,          // BE가 생성해서 넘겨준 url // 보여주기만 하고 다시 POST
+//            "token": string                // client가 입력
+//        }
+//        < Resp >
+//        {
+//            "message": string,
+//            "config_id": int,               // workspace_config 튜플의 id // 수정을 위해 반환받아야 함 // get pooling있어서 필요 없을지도?
+//            "workspace_name": string,       // 입력한 그 이름
+//            "register_date": timestamp      // YYYY/MM/DD
+//        }
+
         // config
         workSpace.setWorkspace_name(orgSaasRequest.getWorkspace_name());
 //      workSpace.setAlias(orgSaasRequest.getAlias());  // 용도가 애매해
         workSpace.setSaas_admin_email(orgSaasRequest.getSaas_admin_email());
         workSpace.setWebhook(orgSaasRequest.getWebhook_url());
         workSpace.setToken(orgSaasRequest.getToken());
+        workSpace.setRegister_date(Timestamp.valueOf(LocalDateTime.now()));
         Workspace saveWorkSpace = workspaceRepository.save(workSpace);
         // OrgSaas
+        orgSaas.setConfig(saveWorkSpace.getId());
         orgSaas.setOrg_id(orgSaasRequest.getOrg_id());;
         orgSaas.setSaas_id(orgSaasRequest.getSaas_id());
-        orgSaas.setConfig(saveWorkSpace.getId());
         orgSaas.setSpace_id(String.valueOf(orgSaasRequest.getSpace_id()));  // Setter가 id라 int로 인식한듯?
         OrgSaas saveOrgSaas = orgSaasRepository.save(orgSaas);
 
-        return new OrgSaasResponse(saveWorkSpace.getId(), saveWorkSpace.getWorkspace_name());
+        return new OrgSaasResponse("Success", saveWorkSpace.getId(), saveWorkSpace.getWorkspace_name(), saveWorkSpace.getRegister_date());
 
     }
 
     @Override
     public OrgSaasResponse modifyOrgSaas(OrgSaasRequest orgSaasRequest) {
+        Workspace workspace = new Workspace();
 
-//        OrgSaasConfig orgSaasConfig = new OrgSaasConfig();
-//
-//        orgSaasConfig.setSaas_admin_email(orgSaasRequest.getSaas_admin_email());
-//        orgSaasConfig.setApi_key(orgSaasRequest.getApi_key());
-//        orgSaasConfig.setSaas_alias(orgSaasRequest.getSaas_alias());
-//        orgSaasConfig.setRegister_date(Timestamp.valueOf(LocalDateTime.now()));
-//
-//        OrgSaasConfig updateOrgSaasConfig = orgSaasConfigRepository.save(orgSaasConfig);
-        return null;
+//        < Req >
+//        {
+//            "config_id": int,
+//            "workspace_name": string,
+//            "saas_admin_email": string,
+//            "token": string
+//          "webhook_url": string
+//        }
+//        < Resp >
+//        {
+//            "message": string,
+//            "config_id": int,
+//            "workspace_name": string,
+//            "register_date": ts
+//        }
+
+        if(workspace.getId().equals(orgSaasRequest.getConfig_id())) {
+            workspace.setWorkspace_name(orgSaasRequest.getWorkspace_name());
+            workspace.setSaas_admin_email(orgSaasRequest.getSaas_admin_email());
+            workspace.setToken(orgSaasRequest.getToken());
+//          workspace.setWebhook(orgSaasRequest.getWebhook_url());  // 이걸 수정 할 일이 있을까?
+            workspace.setRegister_date(Timestamp.valueOf(LocalDateTime.now()));     //register_date긴한데 그냥 last_modified~로 이해하면 됨
+            Workspace saveWorkspace = workspaceRepository.save(workspace);
+
+            return new OrgSaasResponse("Success", saveWorkspace.getId(), saveWorkspace.getWorkspace_name(), saveWorkspace.getRegister_date());
+        }
+        else {
+            return new OrgSaasResponse("Failure: Not found by ID", orgSaasRequest.getConfig_id(), null, null);
+        }
     }
 
     @Override
@@ -70,75 +112,6 @@ public class OrgSaasServiceImple implements OrgSaasService {
         return List.of();
     }
 
-//    @Override
-//    public OrgSaasResponse modifyOrgSaas(OrgSaasRequest orgSaasRequest) {
-//        Optional<OrgSaasConfig> optionalOrgSaasConfig = orgSaasConfigRepository.findById(Long.valueOf(orgSaasRequest.getOrg_saas_id()));
-//        // 솔루션 접속 시, org_id는 결정됨
-//        // 여기서는 org_id는 1이라고 가정하고 진행
-//        // FE는 알고있는 org_id+saas_id를 POST
-//        if(optionalOrgSaasConfig.isPresent()) {
-//            OrgSaasConfig orgSaasConfig = optionalOrgSaasConfig.get();
-//
-//            if(orgSaasConfig.getId().equals(orgSaasRequest.getOrg_saas_id()) && orgSaasConfig.getSaas_admin_email().equals(orgSaasRequest.getSaas_admin_email())) {
-//                orgSaasConfig.setSaas_admin_email(orgSaasRequest.getSaas_admin_email());
-//                orgSaasConfig.setApi_key(orgSaasRequest.getApi_key());
-//                orgSaasConfig.setSaas_alias(orgSaasRequest.getSaas_alias());
-//
-//                OrgSaasConfig updateOrgSaasConfig = orgSaasConfigRepository.save(orgSaasConfig);
-//                OrgSaas updateOrgSaas = new OrgSaas();
-//
-//                return new OrgSaasResponse("success", updateOrgSaasConfig.getId(), updateOrgSaas.getStatus(), orgSaasConfig.getWebhook_url(), orgSaasConfig.getSaas_admin_email(), orgSaasConfig.getSaas_alias());
-//            }
-//            else {
-//                return new OrgSaasResponse("failure", "ID & Email Unmatched!");
-//            }
-//        }
-//        else {
-//            return new OrgSaasResponse("failure", "OrgSaas not found for ID: " + orgSaasRequest.getOrg_saas_id());
-//        }
-//    }
-//
-//    @Override
-//    public OrgSaasResponse deleteOrgSaas(OrgSaasRequest orgSaasRequest) {
-//        Optional<OrgSaasConfig> optionalOrgSaasConfig = orgSaasConfigRepository.findById(Long.valueOf(orgSaasRequest.getOrg_saas_id()));
-//
-//        if (optionalOrgSaasConfig.isPresent()) {
-//            OrgSaasConfig orgSaasConfig = optionalOrgSaasConfig.get();
-//
-//            if(orgSaasConfig.getId().equals(orgSaasRequest.getOrg_saas_id()) && orgSaasConfig.getSaas_admin_email().equals(orgSaasRequest.getSaas_admin_email())) {
-//                orgSaasConfigRepository.deleteById(Long.valueOf(orgSaasRequest.getOrg_saas_id()));
-//                return new OrgSaasResponse("success", orgSaasRequest.getOrg_saas_id(), orgSaasRequest.getSaas_admin_email());
-//            }
-//            else {
-//                return new OrgSaasResponse("failure", "OrgSaas not found for ID: " + orgSaasRequest.getOrg_saas_id());
-//            }
-//        }
-//        else {
-//            return new OrgSaasResponse("failure", "OrgSaas not found for ID: " + orgSaasRequest.getOrg_saas_id());
-//        }
-//    }
-//
-//    @Override
-//    public List<OrgSaasResponse> getOrgSaasList() {
-//        return orgSaasRepository.findAll().stream()
-//                .map(orgSaas -> {
-//                    Optional<OrgSaasConfig> configOptional = orgSaasConfigRepository.findById(Long.valueOf(orgSaas.getConfig_file()));
-//                    OrgSaasConfig config = configOptional.orElse(new OrgSaasConfig());
-//
-//                    Optional<Saas> saasOptional = saasRepository.findById(orgSaas.getSaas_id());
-//                    Saas saas = saasOptional.orElse(new Saas());
-//
-//                    return new OrgSaasResponse(
-//                            "success",
-//                            orgSaas.getId(),
-//                            orgSaas.getStatus(),
-//                            config.getWebhook_url(),
-//                            config.getSaas_admin_email(),
-//                            config.getSaas_alias(),
-//                            config.getRegister_date()
-//                    );
-//                })
-//                .collect(Collectors.toList());
-//    }
+
 
 }
