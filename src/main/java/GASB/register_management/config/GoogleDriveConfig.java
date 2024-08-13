e0c7c9663db07d05e79d92872f4f294e155cb560
mandeuk <mandeuk1999@gmail.com>
2024-08-13 19:21:52 +0900

    KAN-342-gd / Feat: GoogleUtil을 위한 config클래스 추가

--- src/main/java/GASB/register_management/config/GoogleDriveConfig.java
+++ src/main/java/GASB/register_management/config/GoogleDriveConfig.java
@@ -1,2 +1,35 @@
-package GASB.register_management.config;public class GoogleDriverConfig {
+package GASB.register_management.config;
+
+import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
+import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets;
+import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
+import com.google.api.client.json.gson.GsonFactory;
+import com.google.api.client.util.store.FileDataStoreFactory;
+import com.google.api.services.drive.DriveScopes;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.List;
+
+@Configuration
+public class GoogleDriveConfig {
+
+    private static final String TOKENS_DIRECTORY_PATH = "tokens";
+    private static final List<String> SCOPES = Arrays.asList(DriveScopes.DRIVE, DriveScopes.DRIVE_METADATA);
+
+    @Bean
+    public GoogleAuthorizationCodeFlow googleAuthorizationCodeFlow() throws Exception {
+        InputStream in = getClass().getResourceAsStream("/credentials.json");
+        GoogleClientSecrets clientSecrets = GoogleClientSecrets.load(GsonFactory.getDefaultInstance(), new InputStreamReader(in));
+
+        return new GoogleAuthorizationCodeFlow.Builder(
+                GoogleNetHttpTransport.newTrustedTransport(), GsonFactory.getDefaultInstance(), clientSecrets, SCOPES)
+                .setDataStoreFactory(new FileDataStoreFactory(new java.io.File(TOKENS_DIRECTORY_PATH)))
+                .setAccessType("offline")
+                .build();
+    }
+
 }
