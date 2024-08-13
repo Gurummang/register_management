fe6b9c906ef054d4600e8042407d4866d34c71cf
mandeuk <mandeuk1999@gmail.com>
2024-08-13 19:20:42 +0900

    KAN-342-gd / Fix: Jpa<Workspace,Long -> Integer>

---
+++ src/main/java/GASB/register_management/config/GoogleDriveConfig.java
@@ -0,0 +1,2 @@
+package GASB.register_management.config;public class GoogleDriverConfig {
+}
--- src/main/java/GASB/register_management/repository/WorkspaceRepository.java
+++ src/main/java/GASB/register_management/repository/WorkspaceRepository.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Repository;
 import java.util.List;
 
 @Repository
-public interface WorkspaceRepository extends JpaRepository<Workspace, Long> {
+public interface WorkspaceRepository extends JpaRepository<Workspace, Integer> {
 
     List<Workspace> findByIdIn(List<Integer> configIds);
 }
