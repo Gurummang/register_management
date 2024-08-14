b4da4d5a368b9a4adefd6719649527ad6efc6e96
mandeuk <mandeuk1999@gmail.com>
2024-08-14 22:51:20 +0900

    KAN-342-gd / Refac: 토큰을 로컬에 저장하지 않도록 수정

--- src/main/java/GASB/register_management/config/GoogleDriveConfig.java
+++ src/main/java/GASB/register_management/config/GoogleDriveConfig.java
@@ -4,7 +4,6 @@ import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
 import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets;
 import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
 import com.google.api.client.json.gson.GsonFactory;
-import com.google.api.client.util.store.FileDataStoreFactory;
 import com.google.api.services.drive.DriveScopes;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -17,7 +16,7 @@ import java.util.List;
 @Configuration
 public class GoogleDriveConfig {
 
-    private static final String TOKENS_DIRECTORY_PATH = "tokens";
+//    private static final String TOKENS_DIRECTORY_PATH = "tokens";
     private static final List<String> SCOPES = Arrays.asList(DriveScopes.DRIVE, DriveScopes.DRIVE_METADATA);
 
     @Bean
@@ -27,7 +26,7 @@ public class GoogleDriveConfig {
 
         return new GoogleAuthorizationCodeFlow.Builder(
                 GoogleNetHttpTransport.newTrustedTransport(), GsonFactory.getDefaultInstance(), clientSecrets, SCOPES)
-                .setDataStoreFactory(new FileDataStoreFactory(new java.io.File(TOKENS_DIRECTORY_PATH)))
+//                .setDataStoreFactory(new FileDataStoreFactory(new java.io.File(TOKENS_DIRECTORY_PATH)))
                 .setAccessType("offline")
                 .build();
     }
