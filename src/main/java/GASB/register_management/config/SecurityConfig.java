20342199ac91cc82b0f417c5301bce97191a3cfb
mandeuk <mandeuk1999@gmail.com>
2024-08-14 14:42:13 +0900

    KAN-342-gd / Feat: Security Config for Google API

---
+++ src/main/java/GASB/register_management/config/SecurityConfig.java
@@ -0,0 +1,31 @@
+package GASB.register_management.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(authorizeRequests ->
+                        authorizeRequests
+                                .requestMatchers("/oauth2/**", "/login**", "/error**", "/api/v1/connect/google-drive/**","/api/v1/org-saas/**").permitAll()
+                                .anyRequest().authenticated()
+                )
+                .oauth2Login(oauth2Login ->
+                        oauth2Login
+                                .loginPage("/login")
+                                .defaultSuccessUrl("/dashboard", true)
+                                .failureUrl("/login?error=true")
+                );
+
+        return http.build();
+    }
+}
