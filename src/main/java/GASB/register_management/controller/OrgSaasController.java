8e6e86ec0a8d8c29deb151d725ca2368bc6c5db4
mandeuk <mandeuk1999@gmail.com>
2024-08-15 16:07:39 +0900

    KAN-342-g / Comment: 불필요한 부분 삭제

--- src/main/java/GASB/register_management/controller/OrgSaasController.java
+++ src/main/java/GASB/register_management/controller/OrgSaasController.java
@@ -3,17 +3,12 @@ package GASB.register_management.controller;
 import GASB.register_management.dto.OrgSaasRequest;
 import GASB.register_management.dto.OrgSaasResponse;
 import GASB.register_management.service.OrgSaasService;
-
 import GASB.register_management.util.GoogleUtil;
-import com.google.api.services.drive.Drive;
-import org.springframework.security.core.parameters.P;
-import org.springframework.web.bind.annotation.*;
 
+import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
-
-
-@CrossOrigin(origins = {"http://localhost:5173","http://127.0.0.1:5173", "http://localhost:8080"})
+@CrossOrigin(origins = {"http://localhost:5173","http://127.0.0.1:5173"})
 @RestController
 @RequestMapping("/api/v1/org-saas")
 public class OrgSaasController {
@@ -39,7 +34,6 @@ public class OrgSaasController {
     @PostMapping("/register")
     public OrgSaasResponse register(@RequestBody OrgSaasRequest orgSaasRequest) throws Exception {
         if(orgSaasRequest.getSaasId() == 6) {
-            System.out.println("1. 시작");
             return googleUtil.starter(orgSaasRequest);
         }
         return orgSaasService.registerOrgSaas(orgSaasRequest);
@@ -60,10 +54,4 @@ public class OrgSaasController {
     public List<OrgSaasResponse> getOrgSaasList(@PathVariable Integer orgId) {
         return orgSaasService.getOrgSaasList(orgId);
     }
-
-//    @PostMapping("/register/gd")
-//    public Drive registerGoogle() throws Exception {
-//        System.out.println("Call: registerGoogle");
-//        return googleUtil.getDriveService();
-//    }
 }
